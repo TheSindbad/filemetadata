8212b6feb28104d9ef5740ec022d454c71f487b1
TheSindbad <HramzovW2003@gmail.com>
2023-03-26 14:06:58 +0000

    fgghf

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
+const express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
-var app = express();
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const app = express()
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public',express.static(process.cwd()+'public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd()+ '/views/index.html')
+})
+
+app.post('/api/fileanalyse', upload.single('upfile'),  (req, res, next)=> {
+  
+    res.json({
+          "name": req.file.originalname,
+          "type": req.file.mimetype,
+          "size": req.file.size
+    })
+})
 
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`App running on PORT ${ PORT }`);
 });
